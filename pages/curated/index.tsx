326f0de1ea2ddc4f8c08c27606cff259a7f46f4a
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-14 14:18:24 -0300

    routes and new pages

---
+++ pages/curated/index.tsx
@@ -0,0 +1,11 @@
+import CuratedGallery from "@/components/Curated/CuratedGallery";
+import CuratedHero from "@/components/Curated/CuratedHero";
+
+export default function CuratedPage() {
+  return (
+    <div className="w-full pb-24">
+      <CuratedHero />
+      <CuratedGallery />
+    </div>
+  );
+}
---
+++ pages/membership/index.tsx
@@ -0,0 +1,5 @@
+export default function MembershipPage() {
+  return (
+    <div className="w-full pb-24 text-3xl text-center md:text-6xl ">Membership</div>
+  );
+}
