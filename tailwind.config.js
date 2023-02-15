7a2b68a6d09e6cb360942dae0c3917a1285fc025
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-15 19:30:36 -0300

    linting and formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     fontFamily: {
-      base: ["Unbounded", "mono"],
+      base: ['Unbounded', 'mono'],
     },
     extend: {},
   },
