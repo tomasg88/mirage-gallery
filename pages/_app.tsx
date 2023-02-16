4f71e78f68d0e46979d70d12472c46ed2aa06f5e
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-16 19:23:06 -0300

    don't use layout when loading sanity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,15 @@ import '@fontsource/unbounded/500.css';
 import '@fontsource/unbounded/600.css';
 import '@fontsource/unbounded/700.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 function App({ Component, pageProps }: AppProps) {
+  // don't load Layout for Sanity CMS
+  const router = useRouter();
+  if (router.pathname.includes('/mg-admin')) {
+    return <Component {...pageProps} />;
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
