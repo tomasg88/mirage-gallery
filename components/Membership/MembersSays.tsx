1ab51d30d949f72f5c8aad49aedb88e45f274ccd
Tomas Gonzalez Soldavini <tomasg88@gmail.com>
2023-02-17 11:01:39 -0300

    add components for membership page (#13)
    Co-authored-by: Sanrodriguez <sanrodriguez@mpf.gov.ar>

---
+++ components/Membership/MembersSays.tsx
@@ -0,0 +1,59 @@
+import Image from 'next/image';
+
+export default function MembersSays() {
+  return (
+    <>
+      <div className="py-20 ">
+        <h3 className="text-2xl">What our members say</h3>
+        <div className="grid gap-12 mt-6 md:grid-cols-2">
+          <div className="p-6 border border-gray-300">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.
+            </p>
+            <div className="flex mt-6">
+              <Image
+                alt="avatar"
+                className="w-12 h-12 rounded-full"
+                height={50}
+                src="/assets/1.jpg"
+                width={50}
+              />
+              <div className="flex flex-col ml-6">
+                <a className="font-medium">@jessbar</a>
+                <span className="text-gray-600">Intelligent member</span>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 border border-gray-300">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.
+            </p>
+            <div className="flex mt-6">
+              <Image
+                alt="avatar"
+                className="w-12 h-12 rounded-full"
+                height={50}
+                src="/assets/9.jpg"
+                width={50}
+              />
+              <div className="flex flex-col ml-6">
+                <a className="font-medium">@catlover</a>
+                <span className="text-gray-600">Sentient member</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/membership/index.tsx
+++ pages/membership/index.tsx
@@ -1,7 +1,19 @@
+import MembershipBenefits from 'components/Membership/Benefits';
+import HeroMembership from 'components/Membership/HeroMembership';
+import MembersSays from 'components/Membership/MembersSays';
+import Tiers from 'components/Membership/Tiers';
+import ViewOrClaim from 'components/Membership/ViewOrClaim';
+
 export default function MembershipPage() {
   return (
-    <div className="w-full pb-24 text-3xl text-center md:text-6xl ">
-      Membership
+    <div className="w-full pb-24">
+      <HeroMembership />
+      <div className="px-2 mx-auto max-w-screen-2xl">
+        <ViewOrClaim />
+        <Tiers />
+        <MembershipBenefits />
+        <MembersSays />
+      </div>
     </div>
   );
 }
