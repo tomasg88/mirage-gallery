53046a1f755007333a5f66d45638f1d15f1c61dd
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-18 15:01:16 -0300

    feature and shared components

--- modules/curated/components/CuratedGallery.tsx
+++ modules/curated/components/CuratedGallery.tsx
@@ -1,24 +1,27 @@
 import Image from 'next/image';
-import { GALLERY } from 'utils/gallery';
+import Link from 'next/link';
 import styles from 'modules/curated/styles/CuratedGallery.module.css';
-import OurDropsFilter from './OurDropsFilter';
+import OurDropsFilter from 'modules/curated/components/OurDropsFilter';
+import { GALLERY } from 'utils/gallery';
 
 export default function CuratedGallery() {
   return (
     <div className="px-6">
       <OurDropsFilter />
       <div className={styles.gallery}>
-        {GALLERY.map((i) => (
-          <div key={i.id}>
-            <Image
-              alt="alt"
-              blurDataURL={`/assets/blur-${i.img}`}
-              height={900}
-              placeholder="blur"
-              quality={100}
-              src={`/assets/${i.img}`}
-              width={1200}
-            />
+        {GALLERY.map((image) => (
+          <div key={image.id}>
+            <Link href="/curated/oneiroscapes">
+              <Image
+                alt="alt"
+                blurDataURL={`/assets/blur-${image.img}`}
+                height={900}
+                placeholder="blur"
+                quality={100}
+                src={`/assets/${image.img}`}
+                width={1200}
+              />
+            </Link>
           </div>
         ))}
       </div>
