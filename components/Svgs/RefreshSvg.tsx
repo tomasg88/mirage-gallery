77f221eeaa091bd3d5e02e452eb641d63e32755f
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-20 18:38:00 -0300

    update prop names

--- components/Svgs/RefreshSvg.tsx
+++ components/Svgs/RefreshSvg.tsx
@@ -14,16 +14,16 @@ export default function RefreshSvg({ className }: RefreshSvgProps) {
       <path
         d="M2.313 4.449A5.336 5.336 0 0 1 12.335 7M1.664 7a5.336 5.336 0 0 0 10.022 2.551"
         stroke="gray"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
       />
       <path
         d="M4.364 4.449H2.006V2.09M9.637 9.551h2.357v2.358"
         stroke="gray"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
       />
     </svg>
   );
