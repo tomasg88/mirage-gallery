cf16e3deac479b7b21592685051704d0220bcfa8
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-20 20:03:55 -0300

    change attr name to be a prop

--- components/Svgs/EmailEmptyIcon.tsx
+++ components/Svgs/EmailEmptyIcon.tsx
@@ -4,7 +4,7 @@ export const EmailEmptyIcon = () => {
   return (
     <svg fill="none" height="12" width="14" xmlns="http://www.w3.org/2000/svg">
       <path
-        clip-rule="evenodd"
+        clipRule="evenodd"
         d="m11.497 4.96.761-.515c.467-.316.747-.844.747-1.409v0c0-.94-.762-1.703-1.702-1.703H2.707c-.94 0-1.702.762-1.702 1.703v0c0 .565.28 1.093.747 1.41l.76.515a8.007 8.007 0 0 0 8.985 0v0Z"
         stroke="#000"
         strokeLinecap="round"
