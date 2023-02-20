049a25b30df4c0f3ee85b536a76e668e9440d86a
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-20 19:42:17 -0300

    refactor for code readability

--- modules/dreamers/components/DreamersHowItWorks.tsx
+++ modules/dreamers/components/DreamersHowItWorks.tsx
@@ -1,53 +1,24 @@
 import Image from 'next/image';
-
+const STEPS = [
+  'Each month a cause is decided on with input from the MG community and a theme is chosen.',
+  'Dreamers holders are able to submit one artwork per Dreamer before a set deadline',
+  'MG holders vote and decide on 10 favorite artworks',
+  'MG Curated artists & MG team pick the winner',
+  'Winning artwork is minted and put for auction. 10% goes to the artist, and the rest is donated to the cause.',
+];
 export default function DreamersHowItWorks() {
   return (
     <>
       <div className="grid w-full grid-cols-2 py-20 ">
         <ul className="flex flex-col space-y-4 text-xl">
-          <li className="flex items-center justify-start">
-            <span className="inline-flex items-center justify-center w-20 h-20 mr-6 border border-gray-300 rounded-full min-w-[5rem]">
-              1
-            </span>
-            <p className="w-full">
-              Each month a cause is decided on with input from the MG community
-              and a theme is chosen.
-            </p>
-          </li>
-          <li className="flex items-center justify-start">
-            <span className="inline-flex items-center justify-center w-20 h-20 mr-6 border border-gray-300 rounded-full min-w-[5rem]">
-              2
-            </span>
-            <p className="w-full">
-              Dreamers holders are able to submit one artwork per Dreamer before
-              a set deadline
-            </p>
-          </li>
-          <li className="flex items-center justify-start">
-            <span className="inline-flex items-center justify-center w-20 h-20 mr-6 border border-gray-300 rounded-full min-w-[5rem]">
-              3
-            </span>
-            <p className="w-full">
-              MG holders vote and decide on 10 favorite artworks
-            </p>
-          </li>
-          <li className="flex items-center justify-start">
-            <span className="inline-flex items-center justify-center w-20 h-20 mr-6 border border-gray-300 rounded-full min-w-[5rem]">
-              4
-            </span>
-            <p className="w-full">
-              MG Curated artists & MG team pick the winner
-            </p>
-          </li>
-          <li className="flex items-center justify-start">
-            <span className="inline-flex items-center justify-center w-20 h-20 mr-6 border border-gray-300 rounded-full min-w-[5rem]">
-              5
-            </span>
-            <p className="w-full">
-              Winning artwork is minted and put for auction. 10% goes to the
-              artist, and the rest is donated to the cause
-            </p>
-          </li>
+          {STEPS.map((step, index) => (
+            <li className="flex items-center justify-start" key={index}>
+              <span className="inline-flex items-center justify-center w-20 h-20 mr-6 border border-gray-300 rounded-full min-w-[5rem]">
+                {index + 1}
+              </span>
+              <p className="w-full">{step}</p>
+            </li>
+          ))}
         </ul>
         <div className="flex items-center justify-center">
           <Image
