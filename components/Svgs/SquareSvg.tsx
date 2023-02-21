f7d863b2dba97f9e59551029047881ffc949e1a3
Tomas Gonzalez Soldavini <tomasg88@gmail.com>
2023-02-21 11:23:05 -0300

    Bugfixes (#19)
    * change attr name to be a prop
    * fix issues with key in .map

--- components/Svgs/SquareSvg.tsx
+++ components/Svgs/SquareSvg.tsx
@@ -12,7 +12,7 @@ export const SquaresSvg = ({ className }: SquaresSvgProps) => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        clip-rule="evenodd"
+        clipRule="evenodd"
         d="M5.249 6.499H2.748c-.691 0-1.25-.56-1.25-1.25V2.746c0-.69.559-1.25 1.25-1.25h2.5c.691 0 1.251.56 1.251 1.25v2.501c0 .691-.56 1.25-1.25 1.25ZM15.253 6.499H10.25C9.56 6.499 9 5.939 9 5.249V2.746c0-.69.56-1.25 1.25-1.25h5.003c.69 0 1.25.56 1.25 1.25v2.501c0 .691-.56 1.25-1.25 1.25Z"
         stroke="#213BC7"
         strokeLinecap="round"
@@ -31,7 +31,7 @@ export const SquaresSvg = ({ className }: SquaresSvgProps) => {
         strokeLinejoin="round"
       />
       <path
-        clip-rule="evenodd"
+        clipRule="evenodd"
         d="M15.252 14.002h-2.5c-.691 0-1.251-.56-1.251-1.25V10.25c0-.69.56-1.25 1.25-1.25h2.502c.69 0 1.25.56 1.25 1.25v2.502c0 .69-.56 1.25-1.25 1.25Z"
         stroke="#213BC7"
         strokeLinecap="round"
