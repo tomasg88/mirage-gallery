80176b855694b5279b727f8ab1280b1ebff2d2ee
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-23 18:13:26 -0300

    setup studio to use cloudinary

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,11 +5,22 @@ import { curated } from 'lib/schemas/curated';
 import { alejandroAndTaylorCollections } from 'lib/schemas/alejandro-and-taylor-collections';
 import { collectionImage } from 'lib/schemas/objects/collectionImage';
 import { projectInfo } from 'lib/schemas/objects/projectInfo';
+import {
+  cloudinaryImageSource,
+  cloudinarySchemaPlugin,
+} from 'sanity-plugin-cloudinary';
 
 export const config = defineConfig({
   basePath: process.env.NEXT_PUBLIC_SANITY_BASE_PATH,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
-  plugins: [deskTool()],
+  form: {
+    image: {
+      // tells Sanity to use Cloudinary as the source for all assets
+      assetSources: [cloudinaryImageSource],
+    },
+  },
+  // cloudinarySchemaPlugin: this plugin tells Sanity to use Cloudinary CDN
+  plugins: [deskTool(), cloudinarySchemaPlugin()],
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
   schema: {
     types: [
