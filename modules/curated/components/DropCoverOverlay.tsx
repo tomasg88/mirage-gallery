1533a48216fa995b0fcbb15f2b3eac18d0e61917
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-28 19:22:54 -0300

    add icon to DropCoverOverlay

--- modules/curated/components/DropCoverOverlay.tsx
+++ modules/curated/components/DropCoverOverlay.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Drop } from 'types/drops';
 import styles from 'modules/curated/styles/CuratedGallery.module.css';
+import { RightArrow } from 'components/Svgs/RightArrow';
 
 type DropCoverOverlayProps = {
   name: Drop['name'];
@@ -10,6 +11,9 @@ type DropCoverOverlayProps = {
 export const DropCoverOverlay = ({ name, status }: DropCoverOverlayProps) => (
   <div className={styles.dropOverlay}>
     <p>{status}</p>
-    <h3>{name}</h3>
+    <div className="flex justify-between items-center">
+      <h3>{name}</h3>
+      <RightArrow className="w-6 h-6" />
+    </div>
   </div>
 );
