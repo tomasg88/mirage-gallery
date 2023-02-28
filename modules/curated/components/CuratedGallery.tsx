9ba4577a1859facbd76e0b80043fa20ce1e0a170
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-28 12:20:05 -0300

    feature drop filtering

--- modules/curated/components/CuratedGallery.tsx
+++ modules/curated/components/CuratedGallery.tsx
@@ -1,26 +1,50 @@
+import { useCallback, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from 'modules/curated/styles/CuratedGallery.module.css';
-import OurDropsFilter from 'modules/curated/components/OurDropsFilter';
-import { GALLERY } from 'utils/gallery';
+import DropsFilter from 'modules/curated/components/DropsFilter';
+import { Drop, MintStatus } from 'types/drops';
+import { CURATED_DROP_DETAILS } from 'utils/routes';
 
-export default function CuratedGallery() {
+type CuratedGalleryProps = {
+  drops: Drop[];
+};
+
+export default function CuratedGallery({ drops }: CuratedGalleryProps) {
+  const [status, setStatus] = useState<MintStatus>(MintStatus.ALL);
+
+  const filterDrops = useCallback(
+    (drop: Drop) => {
+      if (status === MintStatus.ALL) {
+        return drop;
+      }
+      return drop.status === status;
+    },
+    [status]
+  );
+
   return (
     <div className="px-6">
-      <OurDropsFilter />
+      <DropsFilter setFilter={setStatus} />
       <div className={styles.gallery}>
-        {GALLERY.map((image) => (
-          <div key={image.id}>
-            <Link href="/curated/oneiroscapes">
-              <Image
-                alt="alt"
-                blurDataURL={`/assets/blur-${image.img}`}
-                height={900}
-                placeholder="blur"
-                quality={100}
-                src={`/assets/${image.img}`}
-                width={1200}
-              />
+        {drops.filter(filterDrops).map((drop) => (
+          <div key={drop._id}>
+            <Link href={`${CURATED_DROP_DETAILS}/${drop.slug}`}>
+              {drop.sampleImages?.length > 0 ? (
+                <Image
+                  alt="alt"
+                  height={900}
+                  quality={100}
+                  src={drop.sampleImages[0].url}
+                  width={1200}
+                />
+              ) : (
+                // TODO - show default image if drop has no cover
+                <>
+                  <h3>{drop.name}</h3>
+                  <p>{drop.status}</p>
+                </>
+              )}
             </Link>
           </div>
         ))}
