4b68f6d23ab637f508083f7ac9920555838ca088
Tomas Gonzalez <tomasg88@gmail.com>
2023-02-28 17:31:39 -0300

    initial drops query for /curated page

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,4 +1,12 @@
-/*
-test query to prevent build fail
-*/
-export const query = '';
+import { groq } from 'next-sanity';
+
+/**
+ * Fetch all drops to fill Curated page
+ */
+export const dropsQuery = groq`
+  *[_type == "drop"] {
+    ...,
+    "cover": sampleImages[0].url,
+    "slug": slug.current,
+  }
+`;
