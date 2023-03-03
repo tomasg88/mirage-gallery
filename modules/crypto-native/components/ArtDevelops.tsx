e3d01dbba3fbbe0be45d717fb03318e5e9eb1270
Sanrodriguez <sanrodriguez@mpf.gov.ar>
2023-03-02 23:08:46 -0300

    parallax in array | status text | curated gallery 4 img height

--- modules/crypto-native/components/ArtDevelops.tsx
+++ modules/crypto-native/components/ArtDevelops.tsx
@@ -1,42 +1,29 @@
+const IMAGES = [
+  {
+    url: 'url("/assets/crypto-native/crypto-native-01.jpg")',
+  },
+  {
+    url: 'url("/assets/crypto-native/crypto-native-02.jpg")',
+  },
+  {
+    url: 'url("/assets/crypto-native/crypto-native-03.jpg")',
+  },
+  {
+    url: 'url("/assets/crypto-native/crypto-native-04.jpg")',
+  },
+];
+
 export default function CryptoNativeArtDevelops() {
   return (
     <div>
-      <div className="w-full min-h-screen ">
-        <div
-          className="relative flex items-center justify-center w-full min-h-screen mx-auto bg-fixed bg-center bg-no-repeat md:bg-contain"
-          style={{
-            backgroundImage:
-              'url("/assets/crypto-native/crypto-native-01.jpg")',
-          }}
-        ></div>
-      </div>
-      <div className="w-full min-h-screen ">
-        <div
-          className="relative flex items-center justify-center w-full min-h-screen mx-auto bg-fixed bg-center bg-no-repeat md:bg-contain"
-          style={{
-            backgroundImage:
-              'url("/assets/crypto-native/crypto-native-02.jpg")',
-          }}
-        ></div>
-      </div>
-      <div className="w-full min-h-screen ">
-        <div
-          className="relative flex items-center justify-center w-full min-h-screen mx-auto bg-fixed bg-center bg-no-repeat md:bg-contain"
-          style={{
-            backgroundImage:
-              'url("/assets/crypto-native/crypto-native-03.jpg")',
-          }}
-        ></div>
-      </div>
-      <div className="w-full min-h-screen ">
-        <div
-          className="relative flex items-center justify-center w-full min-h-screen mx-auto bg-fixed bg-center bg-no-repeat md:bg-contain"
-          style={{
-            backgroundImage:
-              'url("/assets/crypto-native/crypto-native-04.jpg")',
-          }}
-        ></div>
-      </div>
+      {IMAGES.map(({ url }) => (
+        <div className="w-full min-h-screen" key={url}>
+          <div
+            className="relative flex items-center justify-center w-full min-h-screen mx-auto bg-fixed bg-center bg-no-repeat md:bg-contain"
+            style={{ backgroundImage: `${url}` }}
+          />
+        </div>
+      ))}
     </div>
   );
 }
