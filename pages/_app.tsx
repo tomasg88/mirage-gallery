48f02b53f8e992f91a2fdd1eb1cf46a2abfded35
Tomas Gonzalez <tomasg88@gmail.com>
2023-03-07 01:15:46 -0300

    setup web3 libs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,10 @@
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 import { Layout } from 'components/Layout/Layout';
 import 'styles/globals.css';
 import 'styles/offcanvas.css';
@@ -7,8 +14,24 @@ import '@fontsource/unbounded/300.css';
 import '@fontsource/unbounded/500.css';
 import '@fontsource/unbounded/600.css';
 import '@fontsource/unbounded/700.css';
-import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
+import '@rainbow-me/rainbowkit/styles.css';
+
+const apiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string;
+
+const { chains, provider } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Mirage Gallery',
+  chains,
+});
+
+const wagmiClient = createClient({
+  connectors,
+  provider,
+});
 
 function App({ Component, pageProps }: AppProps) {
   // don't load Layout for Sanity CMS
@@ -18,9 +41,13 @@ function App({ Component, pageProps }: AppProps) {
   }
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
