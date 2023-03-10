678949eb10f5bad526a10e7381acb59efb105bf3
Tomas Gonzalez <tomasg88@gmail.com>
2023-03-10 19:11:21 -0300

    new query for heros in home

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,6 +1,23 @@
 import { groq } from 'next-sanity';
 
 /**
+ * Fetch drops to show in Home Hero
+ *  - Only 5
+ *  - Ideally in Upcoming || Minting status
+ *  - order by date
+ *  - Only required fields for hero
+ */
+export const dropsHomeHeroQuery = groq`
+  *[_type == "drop" && defined(slug)] {
+    name,
+    "slug": slug.current,
+    "cover": sampleImages[0],
+    "artists": artists[].name,
+      status
+  } [0...5] | order(_createdAt desc)
+`;
+
+/**
  * Fetch all drops to fill Curated page
  */
 export const dropsQuery = groq`
