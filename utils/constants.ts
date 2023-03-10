a0d69b62ccb05ea11e0fb77a70c300c967b04784
Tomas Gonzalez <tomasg88@gmail.com>
2023-03-10 17:48:47 -0300

    update marketplaceUrl in schemas and types

---
+++ utils/constants.ts
@@ -0,0 +1,10 @@
+// These urls are the same for the entire projects
+// Only dynamic that comes from CMS is in the Drops model, [slug].tsx page
+export const MARKETPLACE_URLS = {
+  alejandroAndTaylor: 'https://opensea.io/collection/mirage-gallery',
+  cryptoNative: 'https://opensea.io/collection/crypto-native',
+  curated: 'https://opensea.io/category/mirage-gallery-curated',
+  dreamers: 'https://opensea.io/collection/mirage-gallery-dreamers',
+  dreamingOfABetterWorld: 'https://opensea.io/collection/dreamers-dreaming',
+  membership: 'https://opensea.io/collection/mirage-gallery-memberships',
+};
