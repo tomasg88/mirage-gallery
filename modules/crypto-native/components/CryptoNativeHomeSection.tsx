fc0e4a19fd2da46a148cf46b404a52b30eae25c1
Tomas Gonzalez <tomasg88@gmail.com>
2023-03-11 11:19:28 -0300

    map home sections to constants

--- modules/crypto-native/components/CryptoNativeHomeSection.tsx
+++ modules/crypto-native/components/CryptoNativeHomeSection.tsx
@@ -4,32 +4,33 @@ import { HomeSectionImagesRow } from 'components/Home/HomeSectionImagesRow';
 import Link from 'next/link';
 import React from 'react';
 import { Modules } from 'types/main';
+import { HOME_INFO_BY_MODULE } from 'utils/constants';
 
 export const CryptoNativeHomeSection = () => {
+  const { ctaButton, href, name, paragraph, samples } =
+    HOME_INFO_BY_MODULE[Modules.CRYPTO_NATIVE];
+
   return (
     <section>
-      <HeroTitle accentColor={'text-native'} subtitle="CRYPTO NATIVE">
+      <HeroTitle accentColor={'text-native'} subtitle={name.toUpperCase()}>
         With{' '}
         <Link className="underline text-native" href="/crypto-native">
           Crypto-Native
         </Link>
         , the artwork evolves when a secondary sale happens.
       </HeroTitle>
-      <p className="mx-auto text-lg text-gray-600 md:text-3xl ">
-        Instead of just trading the same JPEG back and forth, you only get to
-        own a unique step in the 10 phases of evolution for a piece.
-      </p>
+      <p className="mx-auto text-lg text-gray-600 md:text-3xl ">{paragraph}</p>
       <div className="relative">
         <div className="absolute top-0 bottom-0 left-0 w-96 bg-gradient-to-r from-white via-white/30 to-transparent" />
         <div className="absolute top-0 bottom-0 right-0 w-96 bg-gradient-to-l from-white via-white/30 to-transparent" />
-        <HomeSectionImagesRow module={Modules.CRYPTO_NATIVE} />
+        <HomeSectionImagesRow samples={samples} />
       </div>
       <div className="flex items-center justify-start mt-6">
         <ButtonLink
           className="flex items-center justify-center px-6 py-4 text-white bg-native"
-          href="/crypto-native"
+          href={href}
           showRightArrow
-          text="Explore crypto native"
+          text={ctaButton}
         />
       </div>
     </section>
--- modules/dreamers/components/DreamersHomeSection.tsx
+++ modules/dreamers/components/DreamersHomeSection.tsx
@@ -3,29 +3,30 @@ import { HeroTitle } from 'components/HeroTitle/HeroTitle';
 import { HomeSectionImagesRow } from 'components/Home/HomeSectionImagesRow';
 import React from 'react';
 import { Modules } from 'types/main';
+import { HOME_INFO_BY_MODULE } from 'utils/constants';
 
 export const DreamersHomeSection = () => {
+  const { ctaButton, href, name, paragraph, samples } =
+    HOME_INFO_BY_MODULE[Modules.DREAMERS];
+
   return (
     <section>
-      <HeroTitle accentColor={'text-dreamers'} subtitle="Dreamers">
+      <HeroTitle accentColor={'text-dreamers'} subtitle={name}>
         The artistic intersection of humans and artificial intelligence. We are{' '}
         <span className="underline text-dreamers">Dreamers.</span>
       </HeroTitle>
-      <p className="mx-auto text-lg text-gray-600 md:text-3xl ">
-        Each Dreamer is the combination of traditional fine-art, machine
-        learning, and collectibles.
-      </p>
+      <p className="mx-auto text-lg text-gray-600 md:text-3xl ">{paragraph}</p>
       <div className="relative">
         <div className="absolute top-0 bottom-0 left-0 w-96 bg-gradient-to-r from-white via-white/30 to-transparent" />
         <div className="absolute top-0 bottom-0 right-0 w-96 bg-gradient-to-l from-white via-white/30 to-transparent" />
-        <HomeSectionImagesRow module={Modules.DREAMERS} />
+        <HomeSectionImagesRow samples={samples} />
       </div>
       <div className="flex items-center justify-start mt-6">
         <ButtonLink
           className="flex items-center justify-center px-6 py-4 text-white bg-dreamers"
-          href="/dreamers"
+          href={href}
           showRightArrow
-          text="Explore dreamers"
+          text={ctaButton}
         />
       </div>
     </section>
