432bbdfac28856c21ce0f149c321cbd24ec0e405
Tomas Gonzalez <tomasg88@gmail.com>
2023-03-11 12:31:07 -0300

    membership home section pulling from constants

--- modules/membership/components/MembershipsHomeSection.tsx
+++ modules/membership/components/MembershipsHomeSection.tsx
@@ -1,9 +1,12 @@
+import { ButtonLink } from 'components/Button/ButtonLink/ButtonLink';
 import { HeroTitle } from 'components/HeroTitle/HeroTitle';
-import { RightArrow } from 'components/Svgs/RightArrow';
-import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
+import { MembershipTiers } from 'types/main';
+import { MEMBERSHIP_MODULE_INFO } from 'utils/constants';
 
 export const MembershipsHomeSection = () => {
+  const { ctaButton, href, tiers } = MEMBERSHIP_MODULE_INFO;
   return (
     <section>
       <HeroTitle accentColor={'text-membership'} subtitle="Memberships">
@@ -12,23 +15,28 @@ export const MembershipsHomeSection = () => {
         give early access to curated drops, and more.
       </HeroTitle>
       <div className="grid gap-12 md:grid-cols-2">
-        <div>
-          <div className="w-full bg-gray-900 h-96"></div>
-          <h3 className="mt-3 text-2xl">Intelligent member</h3>
-        </div>
-        <div>
-          <div className="w-full bg-gray-900 h-96"></div>
-          <h3 className="mt-3 text-2xl">Sentient member</h3>
-        </div>
+        {Object.keys(tiers).map((tierName) => (
+          <div key={tierName}>
+            <div className="w-full relative bg-gray-900 h-96">
+              <Image
+                alt={tiers[tierName as MembershipTiers].imageAlt}
+                fill
+                src={tiers[tierName as MembershipTiers].image}
+              />
+            </div>
+            <h3 className="mt-3 text-2xl">
+              {tiers[tierName as MembershipTiers].name}
+            </h3>
+          </div>
+        ))}
       </div>
       <div className="flex items-center justify-start mt-12 mb-32">
-        <Link
-          className="flex items-center justify-center px-6 py-4 bg-membership"
-          href="/membership"
-        >
-          Explore memberships
-          <RightArrow className="ml-2" stroke={'#000'} />
-        </Link>
+        <ButtonLink
+          className="flex items-center justify-center px-6 py-4 text-white bg-membership"
+          href={href}
+          showRightArrow
+          text={ctaButton}
+        />
       </div>
     </section>
   );
