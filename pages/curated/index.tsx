e0bf2712fe7426cc23b8db940db99d36d9de338d
Tomas Gonzalez <tomasg88@gmail.com>
2023-03-13 14:06:34 -0300

    mint any project section in /curated

--- pages/curated/index.tsx
+++ pages/curated/index.tsx
@@ -3,21 +3,40 @@ import { sanityClient } from 'lib/sanity.client';
 import { CuratedArtistForm } from 'modules/curated/components/CuratedArtistForm';
 import { CuratedGallery } from 'modules/curated/components/CuratedGallery';
 import CuratedHero from 'modules/curated/components/CuratedHero';
-import MintProject from 'modules/curated/components/MintProject';
+import {
+  MAX_RANDOM_PROJECTS,
+  MintRandomProject,
+} from 'modules/curated/components/MintRandomProject';
 import { GetServerSideProps, GetServerSidePropsResult } from 'next';
-import { Drop } from 'types/drops';
+import { useMemo } from 'react';
+import { Drop, MintStatus } from 'types/drops';
 
 type CuratedPageProps = {
   drops: Drop[];
 };
 
 export default function CuratedPage({ drops }: CuratedPageProps) {
+  // Filter 'only 4' & 'currently minting' projects
+  const currentlyMintingProjects: Drop[] = useMemo(
+    () =>
+      drops
+        .filter((drop) => {
+          if (drop.status === MintStatus.MINTING) {
+            return drop;
+          }
+        })
+        .slice(0, MAX_RANDOM_PROJECTS),
+    [drops]
+  );
+
   return (
     <div className="w-full pb-24">
       <CuratedHero />
       <CuratedGallery drops={drops} />
       <CuratedArtistForm />
-      <MintProject />
+      {currentlyMintingProjects.length > 0 && (
+        <MintRandomProject mintingDrops={currentlyMintingProjects} />
+      )}
     </div>
   );
 }
