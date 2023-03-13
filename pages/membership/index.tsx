a9d32761ceac73995133d3a29e313755202091b0
Tomas Gonzalez Soldavini <tomasg88@gmail.com>
2023-03-13 10:26:58 -0300

    Membership Sentient Claim tab (#64)
    * Update README.md
    
    * add selected prop for active tab (#41)
    
    * add selected prop for active tab
    
    * Tabs titles as array
    
    ---------
    
    Co-authored-by: Sanrodriguez <sanrodriguez@mpf.gov.ar>
    
    * home hero swiper with trackpad | parallax cryptonative (#39)
    
    * home hero swiper with trackpad | parallax cryptonative
    
    * parallax in array | status text | curated gallery 4 img height
    
    ---------
    
    Co-authored-by: Sanrodriguez <sanrodriguez@mpf.gov.ar>
    
    * Connect Wallet (#43)
    
    * rename component
    
    * add web3 libs
    
    * setup web3 libs
    
    * types
    
    * connect wallet button and wrapper
    
    * implement button wrapper
    
    * Alejandro and Taylor page (#44)
    
    * query and types
    
    * map cms data
    
    * add collectionName
    
    * add collection string
    
    * Curated Artist form (#46)
    
    * new schema for website submissions
    
    * add sanity create token
    
    * working form saving in cms
    
    * remove console.log
    
    * Add release date (#47)
    
    * package date-fns
    
    * add releaseDate to Sanity
    
    * use releaseDate in website
    
    * pass specific properties to components
    
    * add membership sentient claim form with headlessUI select
    
    * use custom color name
    
    ---------
    
    Co-authored-by: santuan <smrcouto88@gmail.com>
    Co-authored-by: Sanrodriguez <sanrodriguez@mpf.gov.ar>

--- pages/membership/index.tsx
+++ pages/membership/index.tsx
@@ -3,17 +3,75 @@ import { HeroMembership } from 'modules/membership/components/HeroMembership';
 import { MembersSays } from 'modules/membership/components/MembersSays';
 import { Tiers } from 'modules/membership/components/Tiers';
 import { ViewOrClaim } from 'modules/membership/components/ViewOrClaim';
+import { Fragment } from 'react';
+import { Tab } from '@headlessui/react';
+import MembershipSentientClaim from 'modules/membership/components/SentientClaim';
+
+const TABS = [
+  {
+    title: 'About',
+  },
+  {
+    title: 'Sentient claim',
+  },
+];
 
 export default function MembershipPage() {
   return (
     <div className="w-full pb-24">
       <HeroMembership />
-      <div className="px-2 mx-auto max-w-screen-2xl">
-        <ViewOrClaim />
-        <Tiers />
-        <Benefits />
-        <MembersSays />
-      </div>
+      <Tab.Group defaultIndex={0}>
+        <Tab.List className="mt-24 border-b outline-none">
+          <div className="flex items-center justify-start gap-6 px-2 py-3 mx-auto text-xs md:text-base max-w-screen-2xl md:gap-12">
+            {TABS.map(({ title }) => (
+              <Tab as={Fragment} key={title}>
+                {({ selected }) => (
+                  <button
+                    className={`${
+                      selected ? 'text-membership' : ''
+                    } outline-none `}
+                    type="button"
+                  >
+                    {title}
+                  </button>
+                )}
+              </Tab>
+            ))}
+          </div>
+        </Tab.List>
+        <Tab.Panels className="pt-12 ">
+          <Tab.Panel>
+            <div className="px-2 mx-auto max-w-screen-2xl">
+              <ViewOrClaim />
+              <Tiers />
+              <Benefits />
+              <MembersSays />
+            </div>
+          </Tab.Panel>
+          <Tab.Panel>
+            <div className="grid gap-6 px-2 mx-auto max-w-screen-2xl md:grid-cols-2 ">
+              <div className="max-w-xl space-y-6 text-2xl font-medium font-base">
+                <p>
+                  This page is for
+                  <span className="underline"> Sentient Members</span> to claim
+                  their token from Mirage Gallery Curated drops OR for
+                  individuals to check status of existing memberships.
+                </p>
+                <p>
+                  Membership IDs are the token ID of the Sentient membership NFT
+                  (0-49)
+                </p>
+                <p>
+                  Each Sentient membership can only claim 1 piece per curated
+                  project, but can claim that piece at any point in time (even
+                  if everything is sold out).
+                </p>
+              </div>
+              <MembershipSentientClaim />
+            </div>
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 }
